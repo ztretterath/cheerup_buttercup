3efbed78996a722adf3957206487ade02cfa5866
Zach Retterath <ztretterath@gmail.com>
2016-11-02 15:20:31 -0400

    Add procfile for deployment on heroku

--- public/js/railsController.js
+++ public/js/railsController.js
@@ -6,9 +6,15 @@
     var self = this;
     var rootUrl = "http://localhost:3000"
 
-    $http.get(`${rootUrl} + cheerups`)
+    $http.get(`${rootUrl} + /cheerups`)
     .then(function(response){
       console.log(response.data);
+      // Will include a state.go to profile state
     })
+    .catch(function(error){
+      console.log('error', error);
+    })
+
+
   }
 })()
