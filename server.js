875c22db933fa6cfa20e3070d0c4ea929edf6ae1
Zach Retterath <ztretterath@gmail.com>
2016-11-02 14:58:19 -0400

    Fix script tag in index to instantiate angular

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app     = express();
 var path    = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('index');
