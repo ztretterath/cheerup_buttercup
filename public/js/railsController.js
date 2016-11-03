6b83de6c279f1c952b1eb0405a8bba334ac223f5
Zach Retterath <ztretterath@gmail.com>
2016-11-03 12:22:47 -0400

    Add login and logout routes in front end controller

--- public/js/railsController.js
+++ public/js/railsController.js
@@ -39,6 +39,46 @@
         method: 'POST',
         data: user
       })
+      .then(function(response){
+        console.log(response);
+      })
+      .catch(function(error){
+        console.log('ERROR', error);
+      })
+    }
+
+    // This method will hit the rails API
+    // for the login route and log in the
+    // user with jwt
+    this.login = function(user){
+      return $http({
+        url: 'http://localhost:3000/users/login'
+        method: 'POST'
+        data: user
+      })
+      .then(function(response){
+        console.log(response);
+      })
+      .catch(function(error){
+        console.log('ERROR', error);
+      })
+    }
+
+    // This method will hit the rails API
+    // for the login route and log out the
+    // user
+    this.logout = function(user){
+      return $http({
+        url: 'http://localhost:3000/users/logout'
+        method: 'DELETE'
+        data: user
+      })
+      .then(function(response){
+        console.log(response);
+      })
+      .catch(function(error){
+        console.log('ERROR', error);
+      })
     }
 
 
