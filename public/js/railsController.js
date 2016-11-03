4627b5d66b1a752345db926168b21c919d253826
Zach Retterath <ztretterath@gmail.com>
2016-11-03 12:02:50 -0400

    Add create user method on front-end rails controller

--- public/js/railsController.js
+++ public/js/railsController.js
@@ -2,20 +2,41 @@
   angular.module('cheerup')
   .controller('railsController', railsController);
 
-  function railsController($http, $state){
+  function railsController($http){
     var self = this;
     var rootUrl = "http://localhost:3000"
 
-    $http.get(`${rootUrl} + /cheerups`)
-    .then(function(response){
-      console.log('SELF', self);
-      console.log(response.data);
-      // Will include a state.go to profile state
-    })
-    .catch(function(error){
-      console.log('error', error);
-    })
 
+    // GETS ALL USERS -> TEST
+    // $http.get(`http://localhost:3000/users`)
+    // .then(function(response){
+    //   self.users = response.data
+    //   // console.log('SELF', self);
+    //   // console.log(response.data);
+    //   // Will include a state.go to profile state
+    // })
+    // .catch(function(error){
+    //   console.log('error', error);
+    // })
+
+    // $http.get(`http://localhost:3000/cheer_ups`)
+    // .then(function(response){
+    //   // self.cheerups = response.data
+    //   console.log('SELF', self);
+    //   console.log(response.data);
+    //   // Will include a state.go to profile state
+    // })
+    // .catch(function(error){
+    //   console.log('error', error);
+    // })
+
+    this.createUser = function(user) {
+      return $http({
+        url: 'http://localhost:3000/users',
+        method: 'POST',
+        data: user
+      })
+    }
 
   }
 })()
