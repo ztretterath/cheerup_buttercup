fadc9d36d76b75735658b6ba0d42fed83a3638ee
Zach Retterath <ztretterath@gmail.com>
2016-11-04 12:48:54 -0400

    Pass data from login to profile state

--- public/js/railsController.js
+++ public/js/railsController.js
@@ -2,9 +2,10 @@
   angular.module('cheerup')
   .controller('railsController', railsController);
 
-  function railsController($http, $state){
+  function railsController($http, $state, $stateParams){
     var self = this;
-    // var rootUrl = "http://localhost:3000"
+    self.name = "zach"
+    var rootUrl = "http://localhost:3000"
 
 
     // GETS ALL USERS -> TEST
@@ -74,7 +75,7 @@
     // new user
     this.createUser = function(user) {
       return $http({
-        url: 'http://localhost:3000/users',
+        url: `${rootUrl}/users`,
         method: 'POST',
         data: user
       })
@@ -91,35 +92,53 @@
     // user with jwt
     this.login = function(user){
       return $http({
-        url: 'http://localhost:3000/users/login',
+        url: `${rootUrl}/users/login`,
         method: 'POST',
         data: user
       })
       .then(function(response){
-        $state.go('profile', {url:'/profile'})
+        self.currentUser = response.data.user
+        console.log(self.currentUser)
+        $state.go('profile', {url:'/profile', currentUser: self.currentUser})
+        console.log($stateParams);
       })
       .catch(function(error){
-        console.log('ERROR', error);
+        console.log('ERROR ~>', error);
       })
     }
 
     // This method will serve as a
     // helper method to retrieve the
     // data of the logged in user
-    $http.get('http://localhost:3000/users/:id')
+    $http.get(`${rootUrl}/users/:id`)
       .then(function(response){
-        self.user = response.data.user;
+        self.currentUser = response.data.user;
+        console.log(response);
       })
       .catch(function(error){
         console.log('ERROR ~>', error);
       })
+    // this.getUser = function(user){
+    //   return $http({
+    //     url: 'http://localhost:3000/users/:id',
+    //     method: 'GET',
+    //     data: user
+    //   })
+    //   .then(function(response){
+    //     self.currentUser = response.data.user
+    //     console.log(response);
+    //   })
+    //   .catch(function(error){
+    //     console.log(error);
+    //   })
+    // }
 
     // This method will hit the rails API
     // for the login route and log out the
     // user
     this.logout = function(user){
       return $http({
-        url: 'http://localhost:3000/users/logout',
+        url: `${rootUrl}/users/logout`,
         method: 'DELETE',
         data: user
       })
