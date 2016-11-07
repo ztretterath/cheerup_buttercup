793017401e969d43ee603ff2adb444a4e42429b8
Zarela Velasquez <zarelavp@yahoo.com>
2016-11-06 22:46:30 -0500

    Trying to delete a user

--- public/js/railsController.js
+++ public/js/railsController.js
@@ -1,74 +1,141 @@
+// (function(){
+//   angular.module('cheerup')
+//   .controller('railsController', railsController);
+//
+//   function railsController($http, $state, $stateParams){
+//     var self = this;
+//     self.currentUser = JSON.parse(localStorage.getItem('user'));
+//     var rootUrl = "http://localhost:3000";
+//
+//     // This method will hit the rails API
+//     // for the create route and make a
+//     // new user
+//     this.createUser = function(user) {
+//       return $http({
+//         url: `${rootUrl}/users`,
+//         method: 'POST',
+//         data: user
+//       })
+//       .then(function(response){
+//         console.log(response);
+//       })
+//       .catch(function(error){
+//         console.log('ERROR', error);
+//       })
+//     }
+//
+//     // This method will hit the rails API
+//     // for the login route and log in the
+//     // user with jwt
+//     this.login = function(user){
+//       return $http({
+//         url: `${rootUrl}/users/login`,
+//         method: 'POST',
+//         data: user
+//       })
+//       .then(function(response){
+//         localStorage.setItem('user', JSON.stringify(response.data.user));
+//         localStorage.setItem('user', response.data.user);
+//         self.currentUser = JSON.parse(localStorage.getItem('user'));
+//         $state.go('profile', {url:'/profile'});
+//       })
+//       .catch(function(error){
+//         console.log('ERROR ~>', error);
+//       })
+//     }
+//
+//     // This method will hit the rails API
+//     // for the login route and log out the
+//     // user
+//     this.logout = function(user){
+//       localStorage.removeItem('user');
+//       console.log('LOGGED OUT');
+//     }
+//
+//     this.createCheerup = function(cheerup){
+//       debugger;
+//       return $http({
+//         url: `${rootUrl}/users/:id/add_cheer_up`,
+//         method: 'POST',
+//         data: {cheerup: cheerup}
+//       })
+//       .then(function(response){
+//         self.newCheerup = response.config.data.cheerup;
+//         var cheerups = self.currentUser.cheer_ups;
+//         cheerups.unshift(self.newCheerup); // adds to beginning of array
+//         console.log(self.currentUser.cheer_ups);
+//       })
+//       .catch(function(error){
+//         console.log('ERROR ~>', error);
+//       })
+//     }
+//
+//   }
+// })()
+
+
+
 (function(){
   angular.module('cheerup')
-  .controller('railsController', railsController);
-
-  function railsController($http, $state, $stateParams){
+  .controller('railsController', function($http, $state){
     var self = this;
-    self.currentUser = JSON.parse(localStorage.getItem('user'));
     var rootUrl = "http://localhost:3000";
 
-    // This method will hit the rails API
-    // for the create route and make a
-    // new user
     this.createUser = function(user) {
-      return $http({
-        url: `${rootUrl}/users`,
-        method: 'POST',
-        data: user
+    console.log(user);
+    return $http({
+      url: `${rootUrl}/users`,
+      method: 'POST',
+      data: {user: user}
+    })
+    .then(function(response) {
+      console.log(response);
+      if (response.data.status === 200) {
+        console.log('success');
+        self.success = true;
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+  }
+
+  this.login = function(user) {
+    return $http({
+      url: `${rootUrl}/users/login`,
+      method: 'POST',
+      data: {user: user}
+    })
+    .then(function(response) {
+      console.log(response);
+      self.user = response.data.user;
+      self.id = response.data.user.id;
+      // console.log(self.user);
+      console.log('token >>>', response.data.token);
+      // localStorage.setItem('token', JSON.stringify(response.data.token))
+      localStorage.setItem('token',response.data.token)
+          $state.go('profile', {url: '/profile', user: response.data.user})
       })
-      .then(function(response){
-        console.log(response);
-      })
-      .catch(function(error){
-        console.log('ERROR', error);
+      .catch(function(err) {
+        console.log(err);
       })
     }
 
-    // This method will hit the rails API
-    // for the login route and log in the
-    // user with jwt
-    this.login = function(user){
-      return $http({
-        url: `${rootUrl}/users/login`,
-        method: 'POST',
-        data: user
-      })
-      .then(function(response){
-        localStorage.setItem('user', JSON.stringify(response.data.user));
-        localStorage.setItem('user', response.data.user);
-        self.currentUser = JSON.parse(localStorage.getItem('user'));
-        $state.go('profile', {url:'/profile'});
-      })
-      .catch(function(error){
-        console.log('ERROR ~>', error);
-      })
-    }
-
-    // This method will hit the rails API
-    // for the login route and log out the
-    // user
-    this.logout = function(user){
+      this.logout = function(user){
       localStorage.removeItem('user');
       console.log('LOGGED OUT');
     }
 
-    this.createCheerup = function(cheerup){
-      debugger;
-      return $http({
-        url: `${rootUrl}/users/:id/add_cheer_up`,
-        method: 'POST',
-        data: {cheerup: cheerup}
-      })
-      .then(function(response){
-        self.newCheerup = response.config.data.cheerup;
-        var cheerups = self.currentUser.cheer_ups;
-        cheerups.unshift(self.newCheerup); // adds to beginning of array
-        console.log(self.currentUser.cheer_ups);
-      })
-      .catch(function(error){
-        console.log('ERROR ~>', error);
-      })
-    }
-
+  this.destroy = function(user){
+   debugger;
+    return $http({
+      url: `${rootUrl}/users/:id`,
+      method: 'DELETE',
+      data: {user: user}
+    })
   }
+
+
+
+  }); //end of controller
 })()
